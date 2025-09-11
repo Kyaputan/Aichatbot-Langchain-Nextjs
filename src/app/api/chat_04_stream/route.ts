aa0195a0ea862062b23f909a07daed0d2c5cee87
Rachata Singkhet <161452629+Kyaputan@users.noreply.github.com>
2025-09-11 22:37:07 +0700

    Update route.ts

--- src/app/api/chat_04_stream/route.ts
+++ src/app/api/chat_04_stream/route.ts
@@ -15,20 +15,20 @@ const Temperature: number = 0.2
 export async function POST(req: NextRequest) {
 
     try {
-    const { message }: { message: UIMessage[] } = await req.json()
+        const { messages }: { messages : UIMessage[] } = await req.json()
 
-    const llm = new ChatOpenAI({
-        model: process.env.OPENAI_MODEL_NAME || "gpt-4o-mini", // ชื่อโมเดล
-        temperature: Temperature, // ความสร้างสรรค์ของคำตอบ มีระดับ 0-1
-        maxTokens: MaxToken, // จำนวนคำตอบสูงสุดที่ต้องการ
-        // timeout: Timeout, // ระยะเวลาในการรอคำตอบ
-        // maxRetries: MaxRetries, // จำนวนครั้งสูงสุดในการลองใหม่
-        streaming: true, // เปิดใช้ streaming response
-    })
+        const llm = new ChatOpenAI({
+            model: process.env.OPENAI_MODEL_NAME || "gpt-4o-mini", // ชื่อโมเดล
+            temperature: Temperature, // ความสร้างสรรค์ของคำตอบ มีระดับ 0-1
+            maxTokens: MaxToken, // จำนวนคำตอบสูงสุดที่ต้องการ
+            // timeout: Timeout, // ระยะเวลาในการรอคำตอบ
+            // maxRetries: MaxRetries, // จำนวนครั้งสูงสุดในการลองใหม่
+            streaming: true, // เปิดใช้ streaming response
+        })
 
         const prompt = ChatPromptTemplate.fromMessages([
             ["system", "คุณเป็น AI ช่วยเหลือที่ช่วยให้ผู้ใช้สามารถหาข้อมูลที่ต้องการได้"],
-            ...convertToModelMessages(message),        // แปลง UIMessage ให้เป็นรูปแบบที่ LangChain เข้าใจ
+            ...convertToModelMessages(messages),        // แปลง UIMessage ให้เป็นรูปแบบที่ LangChain เข้าใจ
         ])
         const chain = prompt.pipe(llm)
 
@@ -47,13 +47,13 @@ export async function POST(req: NextRequest) {
         console.error("API Error:", error)
         // ส่ง error response กลับไปยัง client
         return new Response(
-          JSON.stringify({
-            error: "An error occurred while processing your request",
-          }),
-          {
-            status: 500, // HTTP status code สำหรับ Internal Server Error
-            headers: { "Content-Type": "application/json" }, // กำหนด content type เป็น JSON
-          }
+            JSON.stringify({
+                error: "An error occurred while processing your request",
+            }),
+            {
+                status: 500, // HTTP status code สำหรับ Internal Server Error
+                headers: { "Content-Type": "application/json" }, // กำหนด content type เป็น JSON
+            }
         )
-      }
     }
+}
