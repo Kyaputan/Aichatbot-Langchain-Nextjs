718937fc587b44c80d67b91d1769a4ae0e148938
Rachata Singkhet <161452629+Kyaputan@users.noreply.github.com>
2025-09-12 20:44:04 +0700

    components redesign

---
+++ src/components/ChatArea/ChatArea.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const ChatArea = () => {
+  return (
+    <div className="flex flex-col justify-center items-center text-center text-gray-500 h-full">
+        <div>
+            <p className="text-lg">👋 สวัสดีครับ!</p>
+            <p className="mt-2">เริ่มการสนทนาได้เลยครับ</p>
+        </div>
+    </div>
+  )
+}
+
+export default ChatArea
